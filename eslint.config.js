defb061366cc90daf6e9d761989e07cdbd7e921a
Cristian <cristi.balan23@gmail.com>
2024-09-13 21:47:36 +0300

    updated eslint.config.js file again

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,19 @@
-module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: 'airbnb-base',
-    parserOptions: {
+// eslint.config.js
+const { defineConfig } = require('eslint-define-config');
+
+module.exports = defineConfig({
+    languageOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
+        globals: {
+            google: true,
+        },
     },
-    globals: {
-        google: true
+    extends: ['airbnb-base'],
+    rules: {
+        'no-param-reassign': 0,
+        'no-shadow': 'off',
+        'no-new': 'off',
     },
     ignorePatterns: [],
-    rules: {
-        "no-param-reassign": 0,
-        "no-shadow": "off",
-        "no-new": "off"
-    },
-};
+});
